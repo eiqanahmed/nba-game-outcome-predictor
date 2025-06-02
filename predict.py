d25647f9a30834fb9f41fd42c489add907bdea58
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2025-06-02 09:35:48 -0400

    Updated code to use the model properly.

--- predict.py
+++ predict.py
@@ -3,11 +3,13 @@ import joblib
 from train_model import predict_upcoming_games  # Make sure this function is accessible
 
 sfs = joblib.load("sfs_model.pkl")
+# model = joblib.load("model.pkl")
 predictors = joblib.load("predictors.pkl")
 
-# Load latest preprocessed data (must match training format)
 full = pd.read_csv("latest_full.csv")
 
-# Run prediction
-predictions = predict_upcoming_games(full, sfs.estimator_, predictors)
+model = sfs.estimator
+
+predictions = predict_upcoming_games(full, model, predictors)
+
 print(predictions)
